6a89944ce89eea3026a06b9b46d82cedb0ce0ed2
scottjparry <13504990+scottjparry@users.noreply.github.com>
2021-04-25 22:58:09 +1000

    Creating a text box with a message

---
+++ Lesson_1_Compiling_and_execution/textbox.java
@@ -0,0 +1,11 @@
+/* This application displays messages in a text box
+This can be useful for warning messages and notifications
+ */
+import javax.swing.JOptionPane;
+
+public class textbox {
+    public static void main(String[] args) {
+        //Display This is a text box in amessage dialog box
+        JOptionPane.showMessageDialog(null,"This is a text box");
+    }
+}
